57a2bbafa7b79a2dc06260ff135b6402bfa1740f
AlexAdrienAuger <alex.adrien.auger@gmail.com>
2018-06-20 09:13:33 +0200

    First version

--- src/App.js
+++ src/App.js
@@ -1,19 +1,135 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import {Container, Button, AppBar, ErrorMessage, ButtonsContainer, PointButton, CalcResult} from "./App.style";
+
+const formatTicketTitle = (ticketTitle) => {
+  var ticketRegex = /\((.*)\)(.*)/g;
+  var match = ticketRegex.exec(ticketTitle);
+  console.log(ticketTitle, match);
+  return {
+    name: match[2],
+    points: parseFloat(match[1])
+  };
+};
 
 class App extends Component {
+  state = {
+    logged: false,
+    boardExist: false,
+    boardId: null,
+    cards: [],
+  };
+
+  authorizeTrello = () => {
+    window.Trello.authorize({
+      type: 'popup',
+      name: 'EstimaPoint',
+      scope: {
+        read: 'true',
+        write: 'true'
+      },
+      expiration: '1day',
+      success: (() => {
+        this.setState({logged: true});
+        this.checkBoard();
+      }),
+      error: (() => {
+        this.setState({logged: false});
+      })
+    });
+  };
+
+  checkBoard = async () => {
+    const me = await window.Trello.members.get("me");
+    const allBoards = await Promise.all(me.idBoards.map((boardId) => window.Trello.boards.get(boardId)));
+    const matchingBoard = allBoards.filter(({name}) => (name.indexOf("[EstimaPoint] AAA") > -1));
+    if (matchingBoard.length >= 0) {
+      this.setState({
+        boardExist: true,
+        boardId: matchingBoard[0].id
+      });
+      this.downloadCards();
+    }
+  };
+
+  downloadCards = async () => {
+    const cards = await window.Trello.get(`boards/${this.state.boardId}/cards`);
+    this.setState({cards});
+  };
+
+  toggleCardState = (cardId) => {
+    const newCards = this.state.cards.map(card => {
+      if (card.id === cardId) {
+        card.clicked = !card.clicked;
+      }
+      return card;
+    });
+    this.setState({cards: newCards});
+  };
+
+  calculateResult = () => (this.state.cards.reduce((counter, currentCard) => {
+    if(currentCard.clicked){
+      const {points} = formatTicketTitle(currentCard.name);
+      return (counter + points);
+    } else {
+      return counter;
+    }
+  }, 0));
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Container>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography variant="title" color="inherit">
+              EstimaPoint
+            </Typography>
+          </Toolbar>
+          {this.state.logged && (
+            <Toolbar>
+              <Typography variant="subheading" color="inherit">
+                Logged
+              </Typography>
+            </Toolbar>
+          )}
+          {this.state.boardExist && this.state.logged && (
+            <Toolbar>
+              <Typography variant="subtitle" color="inherit">
+                Board found
+              </Typography>
+            </Toolbar>
+          )}
+        </AppBar>
+        {!this.state.logged && (
+          <Button variant="contained" onClick={this.authorizeTrello}>
+            Connect to Trello
+          </Button>
+        )}
+        {!this.state.boardExist && this.state.logged && (
+          <ErrorMessage variant="title" color="inherit">
+            Please create a board with this name in your Trello Board :
+            <Typography variant="headline" align="center">[EstimaPoint] AAA</Typography>
+          </ErrorMessage>
+        )}
+        <ButtonsContainer>
+          <CalcResult>
+            <Typography variant="title">{this.calculateResult()}</Typography>
+          </CalcResult>
+          {this.state.cards.map(card => {
+            return (
+              <PointButton
+                variant="contained"
+                key={card.id}
+                onClick={() => this.toggleCardState(card.id)}
+                color={card.clicked ? "secondary" : "primary"}>
+                {card.name}
+              </PointButton>
+            )
+          })}
+        </ButtonsContainer>
+      </Container>
     );
   }
 }
---
+++ src/App.style.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+import MUIButton from '@material-ui/core/Button';
+import MUIAppBar from '@material-ui/core/AppBar';
+import MUITypography from '@material-ui/core/Typography';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
+
+export const CalcResult = styled.div`
+  display: flex;
+  align-self: center;
+  margin: 20px;
+`;
+
+export const Button = styled(MUIButton)`
+  && {
+    margin: auto auto;
+  }
+`;
+
+export const PointButton = styled(MUIButton)`
+  && {
+    margin: 10px auto;
+  }
+`;
+
+export const AppBar = styled(MUIAppBar)`
+  && {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+`;
+
+export const ErrorMessage = styled(MUITypography)`
+  && {
+    margin: auto 20px;
+  }
+`;
+
+export const ButtonsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
